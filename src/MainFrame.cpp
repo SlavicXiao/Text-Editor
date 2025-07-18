2ebb97a2132dbb9e9f5e81a3d999fb3bead63019
Aleksandra Sefer <aleksandra2games@gmail.com>
2025-07-18 18:19:07 +0200

    init

--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -3,6 +3,5 @@
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title)
 {
-    wxPanel* panel = new wxPanel(this); 
-    wxButton* button = new wxButton(panel, wxID_ANY, "My funny button", wxPoint(50,50), wxSize(100,50));
+    wxTextCtrl* textCTRL = new wxTextCtrl(this, wxID_ANY, "", wxPoint(50, 350), wxSize(300, -1));
 };
